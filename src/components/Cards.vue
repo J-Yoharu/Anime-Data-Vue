a6fe3b0e092ec59acf0cdd1f8cce912012781b91
Yoharu <jonathas_a.a@hotmail.com>
2020-08-24 12:40:56 -0300

    Trasformado os cards em components e adicionada tela de 404

---
+++ src/components/Cards.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="min-width:13rem;min-height:16rem;position:relative;" class="card item d-inline-flex m-2 rounded shadow-lg border">
+    <router-link :to="{
+        name:'info',
+        params:{id:data.mal_id},
+        }" tag="a">
+    <img class="card-img-top" :src="data.image_url" alt="">
+    <div class="card-body p-0">
+        <h4 class="card-title mt-2 text-center m-0">{{data.title}}</h4>
+    </div>
+    </router-link>
+</div>
+</template>
+
+<script>
+export default {
+    props:['data'],
+}
+</script>
+
+<style>
+.card img{
+    height:13rem;
+}
+.card-title{
+    font-size:1rem;
+    color:black;
+    overflow: hidden;
+}
+.card:hover{
+    transform: scale(1.1,1.1);
+    cursor:pointer;
+}
+</style>
