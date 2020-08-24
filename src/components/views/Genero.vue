c2c9143f6c9dce3eca93c91fe755494fd86857a9
Yoharu <jonathas_a.a@hotmail.com>
2020-08-24 18:14:58 -0300

    criada página de generos e adicionado imagens no 404

---
+++ src/components/views/Genero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="genre">
+      <h1>Categoria</h1>
+      <div class="container">
+          <div class="row">
+              <Cards :data="data"/>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Cards from '../Cards'
+// https://api.jikan.moe/v3/genre/anime/1
+export default {
+    components:{Cards},
+    props:['id'],
+    methods:{
+        async getGenre(url){
+            let a = await axios.get(url); 
+            this.data = a.data.anime;
+            console.log(this.data);
+        }
+    },
+    data(){
+        return{
+            data:'',
+        }
+    },
+    mounted(){
+        this.getGenre(`https://api.jikan.moe/v3/genre/anime/${this.id}`);
+    }
+}
+
+</script>
+
+<style>
+.genre{
+    color:white;
+}
+
+</style>
